60c0951a92fc72e58e9d0b344188b12f2a0272c3
Gabbu98 <35544491+Gabbu98@users.noreply.github.com>
2025-04-04 16:28:51 +0200

    Create test.go

---
+++ arrays/test.go
@@ -0,0 +1,7 @@
+// find max length of the two lists add extra zeros
+// iterate from end to beginning
+// sum
+// larger than 9 ? use modulus by 10 and include result in i+1
+// repeat
+
+// instead of appemding extra zero leverage catching an error
