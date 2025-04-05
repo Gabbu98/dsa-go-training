4c50b85140604010a6bb8c2022918dee41d3e098
gabbu98 <camillerigabriel43@gmail.com>
2025-04-05 09:35:16 +0000

    completed find duplicate in array

--- arrays/find_duplicate_in_array.go
+++ arrays/find_duplicate_in_array.go
@@ -1,11 +1,22 @@
 package arrays
+
+import "fmt"
+
 /*
 input: list []int
 output: int
 
-Unsorted slice of n positive integers where each number < n
-There is at most one duplicate, return it
+Conditions assumed:
+-> Unsorted slice of n positive integers where each number < n
+-> There is at most one duplicate
+
+Example:
+-> 2, 1 = 1, 2-1 = 1
+-> 3, 1,2 = 3, 3-3 = 0
+-> 4, 1,2,3 = 6, 6-4 = 2 > 1,3,2,3 = 9 9-6 = 3
+-> 5, 1,2,3,4 = 10, 10-5 = 5
 */
+
 
 func findDuplucateInArray(list []int) int {
 	// {2,4,1,3,4} , n = 5
@@ -16,11 +27,33 @@ func findDuplucateInArray(list []int) int {
 		summation = summation + i
 		actual_sum = actual_sum + list[i]
 	}
-
-	return actual_sum-summation
+	var diff int = actual_sum - summation
+	if(diff<=0||diff>=len(list)){
+		return -1
+	}
+	return diff
 }
 
-// 2, 1 = 1, 2-1 = 1
-// 3, 1,2 = 3, 3-3 = 0 
-// 4, 1,2,3 = 6, 6-4 = 2 > 1,3,2,3 = 9 9-6 = 3
-// 5, 1,2,3,4 = 10, 10-5 = 5
+func TestFindDuplicate() {
+	tests := []struct {
+		list	[]int
+		duplicate int
+	}{
+		{[]int{}, -1},
+		{[]int{1, 2, 2}, 2},
+		{[]int{1, 2, 3}, -1},
+		{[]int{1, 1, 2, 3}, 1},
+		{[]int{1, 2, 2, 3}, 2},
+		{[]int{1, 2, 3, 2, 4, 5}, 2},
+		{[]int{3, 2, 1, 4, 5, 4}, 4},
+	}
+	
+	for i, test := range tests {
+		if got := findDuplucateInArray(test.list); got != test.duplicate{
+			fmt.Println("Failed test case #%d. Want %d got %d", i, test.duplicate, got) 
+				
+		}
+	}
+}
+
+
--- main.go
+++ main.go
@@ -7,4 +7,5 @@ import (
 func main() {
     arrays.TestReverseInLine()
 	arrays.TestAddSliceOfTwoNumbers()
+	arrays.TestFindDuplicate()
 }
