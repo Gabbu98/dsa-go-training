c45d93bad74fabb27b8064eabc6c8dbd7ecca7ae
GabrielC <gabrielc@ccbill.com>
2025-05-13 16:19:59 +0100

    nqueens unsolved

--- backtracking/nqueens.go
+++ backtracking/nqueens.go
@@ -1,87 +1,87 @@
-package backtracking
+// package backtracking
 
-import "math"
+// import "math"
 
-// input: an integer representing a n by n chessboard
-// output: all possible arrangements of placing n queens such that the queens do not attack each other
+// // input: an integer representing a n by n chessboard
+// // output: all possible arrangements of placing n queens such that the queens do not attack each other
 
-// checker: cross and x check
-// used 2d array: n by n; when a check is performed it marks all the boxes made unavailable
-// iterate box by box
-func resetBoard(n int) [][]bool {
-	board := make([][]bool, n)
-	for i:=0; i<n; i++ {
-		board[i] = make([]bool, n)
-	}
-	return board
-}
+// // checker: cross and x check
+// // used 2d array: n by n; when a check is performed it marks all the boxes made unavailable
+// // iterate box by box
+// func resetBoard(n int) [][]bool {
+// 	board := make([][]bool, n)
+// 	for i:=0; i<n; i++ {
+// 		board[i] = make([]bool, n)
+// 	}
+// 	return board
+// }
 
-func nqueens_drive(n int) bool {
-	useds := [][][]bool{}
-	board := resetBoard(n)
-	used := make([]bool, n)
-	for i:=0; i<n; i++ {
-		board[i] = make([]bool, n)
-	}
+// func nqueens_drive(n int) bool {
+// 	useds := [][][]bool{}
+// 	board := resetBoard(n)
+// 	used := make([]bool, n)
+// 	for i:=0; i<n; i++ {
+// 		board[i] = make([]bool, n)
+// 	}
 
-	for i:=0; i<n; i++ {
-		board[0][i]=true
-		if !nqueens(n, &board,used) {
-			return false
-		} else {
-			useds = append(useds, board)
-		}
-		board=resetBoard(n)
-	}
+// 	for i:=0; i<n; i++ {
+// 		board[0][i]=true
+// 		if !nqueens(n, &board,used) {
+// 			return false
+// 		} else {
+// 			useds = append(useds, board)
+// 		}
+// 		board=resetBoard(n)
+// 	}
 
-	return true
-}
+// 	return true
+// }
 
-func nqueens(n int, board *[][]bool, used []bool) bool {
-	for row:=0; row<n; row++ {
-		if !used[row] { 
-			used[row] = true
+// func nqueens(n int, board *[][]bool, used []bool) bool {
+// 	for row:=0; row<n; row++ {
+// 		if !used[row] { 
+// 			used[row] = true
 
-			for col:=0; col<n; col++ {
-				if !(*board)[row][col] && !isUsed(row, col, board) {
-					(*board)[row][col] = true
-					if nqueens(n, board, used){
-						return true
-					}
-				}
-			}
+// 			for col:=0; col<n; col++ {
+// 				if !(*board)[row][col] && !isUsed(row, col, board) {
+// 					(*board)[row][col] = true
+// 					if nqueens(n, board, used){
+// 						return true
+// 					}
+// 				}
+// 			}
 
-			used[row] = false
-			return false
-		}
-	}
+// 			used[row] = false
+// 			return false
+// 		}
+// 	}
 
-	return false
-}
+// 	return false
+// }
 
-func isUsed(currentRow int, currentCol int, used *[][]bool) bool {
+// func isUsed(currentRow int, currentCol int, used *[][]bool) bool {
 
-	// check cross sectionaly
-	for row:=0; row<len(*used); row++ {
-		for col:=0; col<len((*used)[row]); col++ {
+// 	// check cross sectionaly
+// 	for row:=0; row<len(*used); row++ {
+// 		for col:=0; col<len((*used)[row]); col++ {
 
-			if col==currentCol {
-				return (*used)[row][col]
-			}
+// 			if col==currentCol {
+// 				return (*used)[row][col]
+// 			}
 
-			if row==currentRow {
-				return (*used)[row][col]
-			}
+// 			if row==currentRow {
+// 				return (*used)[row][col]
+// 			}
 			
-			colDiff:=col-currentCol
-			rowDiff:=row-currentRow
+// 			colDiff:=col-currentCol
+// 			rowDiff:=row-currentRow
 
-			if math.Abs(float64(colDiff)) == math.Abs(float64(rowDiff)) {
-				return (*used)[row][col]
-			}
+// 			if math.Abs(float64(colDiff)) == math.Abs(float64(rowDiff)) {
+// 				return (*used)[row][col]
+// 			}
 
-		}
-	}
-	return false
-}
+// 		}
+// 	}
+// 	return false
+// }
 
