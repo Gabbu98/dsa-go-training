e3cb68e6a560975bf9d9ea707d7faa76de4f266c
GabrielC <gabrielc@ccbill.com>
2025-05-14 19:33:02 +0100

    max_stack completed impl

--- stack/max_stack.go
+++ stack/max_stack.go
@@ -1,6 +1,9 @@
 package stack
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 /*
 Implement a stack that can return the max of element it contains
@@ -41,7 +44,60 @@ func (maxStack *MaxStack) Pop() (int, error) {
 	return value,nil
 }
 
-func (maxStack *MaxStack) Max() int {
-	return maxStack.maxStack[len(maxStack.maxStack)-1]
+func (maxStack *MaxStack) Max() (int,error) {
+	if len(maxStack.maxStack)==0 {
+		return -1,ErrEmptyStack
+	}
+	return maxStack.maxStack[len(maxStack.maxStack)-1],nil
 }
 
+
+/*
+TestMaxStack tests solution(s) with the following signature and problem description:
+
+	func (maxStack *MaxStack) Push(i int)
+	func (maxStack *MaxStack) Max() int
+
+Implement a stack that can return the max of element it contains.
+
+For example if we push {2,4,5} to the stack, max should return 5.
+*/
+func TestMaxStack() {
+	tests := []struct {
+		push []int
+		pop  int
+		max  int
+	}{
+		{[]int{}, 0, -1},
+		{[]int{1, 2, 3, 4, 5}, 2, 3},
+		{[]int{1, 2, 3, 4, 5}, 0, 5},
+		{[]int{5, 4, 3, 2, 1}, 2, 5},
+		{[]int{1, 5, 3, 4}, 1, 5},
+	}
+
+	for i, test := range tests {
+		stack := new(MaxStack)
+		for _, p := range test.push {
+			stack.Push(p)
+		}
+
+		for range test.pop {
+			_, err := stack.Pop()
+			if err != nil {
+				fmt.Printf("unexpected error: %s", err)
+			}
+		}
+
+		got,_ := stack.Max()
+		if got != test.max {
+		fmt.Printf("Failed test case #%d. Want %#v got %#v", i, test.max, got)
+		}
+	}
+}
+
+func TestEmptyStackError() {
+	stack := new(MaxStack)
+	if _, err := stack.Pop(); !errors.Is(err, ErrEmptyStack) {
+		fmt.Printf("Calling Pop on an empty stack did not result in empty stack error. Want %#v got %#v", ErrEmptyStack, err)
+	}
+}
